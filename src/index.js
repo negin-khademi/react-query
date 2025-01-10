ff2a7082ecb5f766f506b6edbcd5cf8beb7a484f
Negin <negin.khademi@gmail.com>
2025-01-10 14:20:27 +0330

    reacr query install and setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import './index.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
