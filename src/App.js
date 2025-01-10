9bf2e9c85c57f8e35894dc4f28549237f2e47a8d
Negin <negin.khademi@gmail.com>
2025-01-10 14:15:59 +0330

    initial project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Form from './Form';
+import Items from './Items';
+import { ToastContainer } from 'react-toastify';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+const defaultItems = [
+  { id: nanoid(), title: 'walk the dog', isDone: false },
+  { id: nanoid(), title: 'wash dishes', isDone: false },
+  { id: nanoid(), title: 'drink coffee', isDone: true },
+  { id: nanoid(), title: 'take a nap', isDone: false },
+];
+const App = () => {
+  const [items, setItems] = useState(defaultItems);
+  return (
+    <section className='section-center'>
+      <ToastContainer position='top-center' />
+      <Form />
+      <Items items={items} />
+    </section>
+  );
+};
+export default App;
